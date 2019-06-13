dca5492f7fcd9aeb9b5790a9ba7cd096d01bd510
sudosktripathi <51773838+sudosktripathi@users.noreply.github.com>
2019-06-13 10:45:47 +0530

    Add files via upload

---
+++ fhirPOC/src/test/java/com/rsi/fhirPOC/FhirPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsi.fhirPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
